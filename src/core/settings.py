f65468fc442a9d0ca550b5c7272f1f46c37a92f9
Douglas de Souza Zapelini <douglasszapelini@gmail.com>
2023-08-16 12:03:33 -0300

    🔧chore/Configurando banco de dados

--- src/core/settings.py
+++ src/core/settings.py
@@ -2,11 +2,12 @@ import secrets
 
 from pydantic import BaseSettings
 
+
 class Settings(BaseSettings):
     API_STR: str = "/api"
     PROJECT_NAME: str = "ProvaGustavo"
     SECRET_KEY: str = secrets.token_urlsafe(32)
-    SQLALCHEMY_DATABASE_URI: str = "postgresql://postgres:senha@localhost:5432/nome_do_banco"
+    SQLALCHEMY_DATABASE_URI: str = "postgresql://postgres:7355@localhost:5432/prova"
 
     class Config:
         env_file = ".env"
